314680df22495abe80b3a5e418b35db3839a285a
Juangaca02 <juanangarciacastro@gmail.com>
2024-02-29 19:11:16 +0100

    feat casino laravel

---
+++ casino/resources/views/home.blade.php
@@ -0,0 +1,241 @@
+<x-app-layout>
+    <div class="bg-image">
+        <div class="overlay">
+            <h2>────── EXPERIENCE THE RICHNESS ──────</h2>
+            <h1>Win Big Playing Luxury Casino Games</h1>
+            <p>Win a once-in-a-lifetime exclusive Experience!</p>
+            <button type="submit" class="btn-dorado  text-white font-medium rounded-lg text-sm px-4 py-2 text-center">Get
+                started</button>
+        </div>
+    </div>
+    <br><br>
+    <div class="centrar grid-container">
+        <div class="mx-auto ">
+            <a class="centrar" href=""><img src="/images/poker.png" alt="" style="height: 100px;"></a>
+            <p class="text-white text-center">Roulette</p>
+            <!-- <table border="1" class=" min-w-4 text-white text-center border border-white"> -->
+            <div class="border border-yellow-500 rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-white">
+                    <thead class="text-xs uppercase dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Product name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Color
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Price
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Option
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                            <td class="px-6 py-4">
+                                <button type="button" (click)="goToLogin()"
+                                    class="btn-dorado text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:focus:ring-blue-800">Get
+                                    started</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                            <td class="px-6 py-4">
+                                <button type="button" (click)="goToLogin()"
+                                    class="btn-dorado text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:focus:ring-blue-800">Get
+                                    started</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                            <td class="px-6 py-4">
+                                <button type="button" (click)="goToLogin()"
+                                    class="btn-dorado text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:focus:ring-blue-800">Get
+                                    started</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="mx-auto ">
+            <a class="centrar" href=""><img src="/images/poker.png" alt="" style="height: 100px;"></a>
+            <p class="text-white text-center">Roulette</p>
+            <!-- <table border="1" class=" min-w-4 text-white text-center border border-white"> -->
+            <div class="border border-yellow-500 rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-white">
+                    <thead class="text-xs uppercase dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Product name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Color
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Price
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="mx-auto ">
+            <a class="centrar" href="">
+                <><img src="/images/poker.png" alt="" style="height: 100px;">
+            </a>
+            <p class="text-white text-center">Roulette</p>
+            <!-- <table border="1" class=" min-w-4 text-white text-center border border-white"> -->
+            <div class="border border-yellow-500 rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-white">
+                    <thead class="text-xs uppercase dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Product name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Color
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Price
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="px-6 py-4 font-medium dorado">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ casino/resources/views/layouts/navigation.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar-fixed text-white py-3 px-4 flex items-center justify-between">
+    <a class="font-bold text-xl tracking-tight ml-6 " href="#">Casino</a>
+    <div class="flex items-center">
+        <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Login</a>
+        <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">About</a>
+    </div>
+</nav>
